3b50502872c7520874a142e2acd83823e41beeb7
jlaw541 <jlawrencebel@gmail.com>
2025-09-21 20:47:39 +0800

    StealAbrainrot.lua

--- StealAbrainrot.lua
+++ StealAbrainrot.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/34dae5ec17fba7cf67b79a4c94e1ea6f"))()
+loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/77cae4e4b08cb0f8ae080e7b55fa9690"))()
