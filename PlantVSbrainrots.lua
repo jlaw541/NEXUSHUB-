f1f74dfb0cf01cb806f7dedec69a72b4b2da13de
jlaw541 <jlawrencebel@gmail.com>
2025-09-29 22:51:13 +0800

    PlantVSbrainrots.lua

--- PlantVSbrainrots.lua
+++ PlantVSbrainrots.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/5481dfd22a116f03a84df32a3eaa92f2"))()
+loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/553efbb37443c2c55829933a83b955cf"))()
